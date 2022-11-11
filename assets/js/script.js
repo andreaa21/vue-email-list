f2742c9515f3c5b12c0efea3e2c20135454e662d
andreaa21 <andrea.zollini@proton.me>
2022-11-11 16:49:53 +0100

    add list of emails

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,32 @@
     **Bonus**
     Creare un loading e far comparire gli indirizzi email solamente quando sono stati TUTTI generati.
  */
+
+
+
+    const {createApp} = Vue;
+
+
+    createApp({
+        data(){
+            return {
+                title: 'Email List',
+                emailList: [],
+                apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            }
+        },
+        methods: {
+            getApi(){
+                for (i = 0; i < 10; i++){
+                    axios.get(this.apiUrl)
+                    .then( result => {
+                        console.log(result.data.response);
+                        this.emailList.push(result.data.response)
+                    })
+                }
+            }
+        },
+        mounted(){
+            this.getApi()
+        }
+    }).mount('#app')
