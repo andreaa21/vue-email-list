5adfeb256a51c1cbcfa74ace59c20eed63c1126b
andreaa21 <andrea.zollini@proton.me>
2022-11-11 14:27:36 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/**
+    Attraverso l’apposita API di Boolean
+    https://flynn.boolean.careers/exercises/api/random/mail
+    generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+    **Bonus**
+    Creare un loading e far comparire gli indirizzi email solamente quando sono stati TUTTI generati.
+ */
